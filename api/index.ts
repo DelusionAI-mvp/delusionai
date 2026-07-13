12a50ce71588cf662f4804c183f80c6cb8692c7a
delusionai <delusionai.in@gmail.com>
2026-07-13 19:28:03 +0530

    Create index.ts

---
+++ api/index.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+import serverApp from '../dist/server.cjs';
+
+// Extract the app instance from the bundled server module (esbuild CJS format compatibility)
+const app = (serverApp as any).default || serverApp;
+
+export default app;
+
